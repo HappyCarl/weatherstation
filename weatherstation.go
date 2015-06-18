92ae3d7a3ab8b350101a28ccada4206ca71efccc
Ole Wehrmeyer <me@belogron.de>
2015-06-18 12:16:24 +0200

    Passes GoLint now
    renamed a bunch of stuff to pass GoLint

--- weatherstation.go
+++ weatherstation.go
@@ -1,196 +1,198 @@
 package main
 
 import (
-  "github.com/tarm/goserial"
-  "code.google.com/p/gcfg"
-  "log"
-  "flag"
-  "bytes"
-  "strings"
-  "strconv"
-  "time"
-  "net/http"
-  "fmt"
-  //owm "./openweathermap"
-  )
+	"bytes"
+	"code.google.com/p/gcfg"
+	"flag"
+	"fmt"
+	"github.com/tarm/goserial"
+	"log"
+	"net/http"
+	"strconv"
+	"strings"
+	"time"
+	//owm "./openweathermap"
+)
 
-//data type for the config file
+//Config is the data type for the config file
 type Config struct {
-  Data struct {
-    Latitude string
-    Longitude string
-  }
-  Communication struct {
-    Port string
-    BaudRate int
-  }
-  OpenWeatherMap struct {
-    StationName string
-    Username string
-    Password string
-  }
-  Webserver struct {
-    Address string
-  }
+	Data struct {
+		Latitude  string
+		Longitude string
+	}
+	Communication struct {
+		Port     string
+		BaudRate int
+	}
+	OpenWeatherMap struct {
+		StationName string
+		Username    string
+		Password    string
+	}
+	Webserver struct {
+		Address string
+	}
 }
 
-var last_update = time.Now()
+var lastUpdate = time.Now()
+
 //Every 1 min a value is saved
-var rain_1h_array [60]int
-var rain_1h_index int = 0
+var rain1hArray [60]int
+var rain1hIndex int
 
-var rain_24h_array [60*24]int
-var rain_24h_index int = 0
+var rain24hArray [60 * 24]int
+var rain24hIndex int
 
-var first_data bool = true
+var firstData = true
 
 //current values, ment to be served by the http server
-var current_temp float64
-var current_humidity float64
-var current_speed float64
-var current_rain bool
-var current_rain_1h float64
-var current_rain_24h float64
-
+var currentTemp float64
+var currentHumidity float64
+var currentSpeed float64
+var currentRain bool
+var currentRain1h float64
+var currentRain24h float64
 
 func main() {
-  var configFile = flag.String("config", "config.gcfg", "Path to Config-File.")
+	var configFile = flag.String("config", "config.gcfg", "Path to Config-File.")
 
-  flag.Parse()
+	flag.Parse()
 
-  //Read and parse the cofig file
-  log.Print("Reading Config...")
-  err, cfg := ParseConfig(*configFile)
+	//Read and parse the cofig file
+	log.Print("Reading Config...")
+	cfg, err := ParseConfig(*configFile)
 
-  if err != nil {
-    log.Fatal(err)
-    return
-  }
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
 
-  log.Print("Config has been read.")
+	log.Print("Config has been read.")
 
-  //start the serial communication
-  go StartCommunication(cfg)
+	//start the serial communication
+	go StartCommunication(cfg)
 
-
-  //start the web server
-  StartWebserver(cfg)
+	//start the web server
+	StartWebserver(cfg)
 }
 
+//StartWebserver starts and initializes the web server to serve the weather data
 func StartWebserver(cfg Config) {
-  log.Print("Starting webserver on " + cfg.Webserver.Address)
-  http.HandleFunc("/data", DataHttpHandler)
-  http.Handle("/", http.FileServer(assetFS()))
-  http.ListenAndServe(cfg.Webserver.Address, nil)
+	log.Print("Starting webserver on " + cfg.Webserver.Address)
+	http.HandleFunc("/data", DataHttpHandler)
+	http.Handle("/", http.FileServer(assetFS()))
+	http.ListenAndServe(cfg.Webserver.Address, nil)
 }
 
-func DataHttpHandler(w http.ResponseWriter, r *http.Request) {
-  w.Header().Set("Access-Control-Allow-Origin", "*")
-  log.Print("HTTP: data requested")
-  fmt.Fprintf(w,"{\"temp\": %.1f,\"humidity\": %.0f,\"wind_speed\": %.1f,\"rain\": {\"h1\": %.1f, \"h24\": %.1f, \"current\": %t }}", current_temp, current_humidity, current_speed, current_rain_1h, current_rain_24h, current_rain)
+//DataHTTPHandler handles the rain data requests
+func DataHTTPHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	log.Print("HTTP: data requested")
+	fmt.Fprintf(w, "{\"temp\": %.1f,\"humidity\": %.0f,\"windSpeed\": %.1f,\"rain\": {\"h1\": %.1f, \"h24\": %.1f, \"current\": %t }}", currentTemp, currentHumidity, currentSpeed, currentRain1h, currentRain24h, currentRain)
 }
 
-
+//StartCommunication opens the serial connection and reads the data from it
 func StartCommunication(cfg Config) {
-  //initialize the serial connection
-  c := &serial.Config{Name: cfg.Communication.Port, Baud: cfg.Communication.BaudRate}
+	//initialize the serial connection
+	c := &serial.Config{Name: cfg.Communication.Port, Baud: cfg.Communication.BaudRate}
 
-  log.Print("Starting Communication")
+	log.Print("Starting Communication")
 
-  //and now open the port
-  s, err := serial.OpenPort(c)
-  if err != nil || s == nil {
-    log.Fatal(err)
-    return
-  }
+	//and now open the port
+	s, err := serial.OpenPort(c)
+	if err != nil || s == nil {
+		log.Fatal(err)
+		return
+	}
 
-  log.Print("Port has been opened.")
+	log.Print("Port has been opened.")
 
-  //endlosschleife
-  for true {
-    var buffer bytes.Buffer
-    stop := false
+	//endlosschleife
+	for true {
+		var buffer bytes.Buffer
+		stop := false
 
-    for !stop {
-      buf := make([]byte, 4)
-      _, err = s.Read(buf)
-      if err != nil {
-          log.Fatal(err)
-      } else {
-        value := string(buf)
-        buffer.WriteString(value)
-        stop = value[0] == '\015'
-      }
-    }
+		for !stop {
+			buf := make([]byte, 4)
+			_, err = s.Read(buf)
+			if err != nil {
+				log.Fatal(err)
+			} else {
+				value := string(buf)
+				buffer.WriteString(value)
+				stop = value[0] == '\015'
+			}
+		}
 
-    data := buffer.String()
-    data = strings.TrimSpace(data)
+		data := buffer.String()
+		data = strings.TrimSpace(data)
 
-    //parse the incoming data
-    Parse(data, cfg)
-  }
+		//parse the incoming data
+		Parse(data, cfg)
+	}
 }
 
+//Convert transformes the string data to the corresponding float64 value
 func Convert(data string) (float64, error) {
-  return strconv.ParseFloat(strings.TrimSpace(strings.Replace(strings.Replace(data, ",", ".", 1), "\x00", "", 42)), 64)
+	return strconv.ParseFloat(strings.TrimSpace(strings.Replace(strings.Replace(data, ",", ".", 1), "\x00", "", 42)), 64)
 }
 
+//Parse thates the received data and parses it
 func Parse(data string, cfg Config) {
-  log.Print("Received Data: ", data)
-  split := strings.Split(data, ";")
+	log.Print("Received Data: ", data)
+	split := strings.Split(data, ";")
 
-  //calculate average temperature
-  temperature_1, err1 := Convert(split[3])
-  temperature_2, err2 := Convert(split[19])
-  temperature         := (temperature_1 + temperature_2) / 2
+	//calculate average temperature
+	temperature1, err1 := Convert(split[3])
+	temperature2, err2 := Convert(split[19])
+	temperature := (temperature1 + temperature2) / 2
 
-  if err1 != nil && err2 == nil {
-    temperature = temperature_1
-  } else if err1 == nil && err2 != nil {
-    temperature = temperature_2
-  }
+	if err1 != nil && err2 == nil {
+		temperature = temperature1
+	} else if err1 == nil && err2 != nil {
+		temperature = temperature2
+	}
 
-  temperature_s := strconv.FormatFloat(temperature, 'f', 16, 64)
+	temperatureString := strconv.FormatFloat(temperature, 'f', 16, 64)
 
-  //calculate average humdity
-  humidity_1, errA    := Convert(split[11])
-  humidity_2, errB    := Convert(split[20])
-  humidity            := (humidity_1 + humidity_2) / 2
+	//calculate average humdity
+	humidity1, errA := Convert(split[11])
+	humidity2, errB := Convert(split[20])
+	humidity := (humidity1 + humidity2) / 2
 
-  if errA != nil && errB == nil {
-    humidity = humidity_1
-  } else if errA == nil && errB != nil {
-    humidity = humidity_2
-  }
+	if errA != nil && errB == nil {
+		humidity = humidity1
+	} else if errA == nil && errB != nil {
+		humidity = humidity2
+	}
 
-  humidity_s := strconv.FormatFloat(humidity, 'f', 16, 64)
+	humidityString := strconv.FormatFloat(humidity, 'f', 16, 64)
 
-  //get the wind speed
-  p, _ := Convert(split[21])
-  wind_speed       := strconv.FormatFloat(p, 'f', 16, 64)
+	//get the wind speed
+	p, _ := Convert(split[21])
+	windSpeed := strconv.FormatFloat(p, 'f', 16, 64)
 
+	//and calculate the rain
+	rainTicks, _ := Convert(split[22])
+	rainTicksString := strconv.FormatFloat(rainTicks, 'f', 16, 64)
 
-  //and calculate the rain
-  rain_ticks, _ := Convert(split[22])
-  rain_ticks_s := strconv.FormatFloat(rain_ticks, 'f', 16, 64)
+	rain1h, rain24h := calculateRain(int(rainTicks))
+	rain1hString := strconv.FormatFloat(rain1h, 'f', 16, 64)
+	rain24hString := strconv.FormatFloat(rain24h, 'f', 16, 64)
+	rain := split[23]
 
-  rain_1h, rain_24h := calculateRain(int(rain_ticks))
-  rain_1h_s := strconv.FormatFloat(rain_1h, 'f', 16, 64)
-  rain_24h_s := strconv.FormatFloat(rain_24h, 'f', 16, 64)
-  rain             := split[23]
+	//owm upload currently not working
+	//owm.Transmit(temperatureString, humidityString, windSpeed, rain1hString, rain24hString, cfg.OpenWeatherMap.StationName, cfg.OpenWeatherMap.Username, cfg.OpenWeatherMap.Password, cfg.Data.Longitude, cfg.Data.Latitude)
 
-  //owm upload currently not working
-  //owm.Transmit(temperature_s, humidity_s, wind_speed, rain_1h_s, rain_24h_s, cfg.OpenWeatherMap.StationName, cfg.OpenWeatherMap.Username, cfg.OpenWeatherMap.Password, cfg.Data.Longitude, cfg.Data.Latitude)
+	//save all the data to serve them via HTTP
+	currentTemp = temperature
+	currentHumidity = humidity
+	currentSpeed = p
+	currentRain = (rain == "1")
+	currentRain1h = rain1h
+	currentRain24h = rain24h
 
-  //save all the data to serve them via HTTP
-  current_temp = temperature
-  current_humidity = humidity
-  current_speed = p
-  current_rain = (rain == "1")
-  current_rain_1h = rain_1h
-  current_rain_24h = rain_24h
-
-  log.Print("Temp: " + temperature_s +" Humidity: " + humidity_s +" WindSpeed: " + wind_speed +" Rain Ticks: " + rain_ticks_s + " Rain 1h: " + rain_1h_s + " Rain 24h: " + rain_24h_s + " Rain: " + rain)
+	log.Print("Temp: " + temperatureString + " Humidity: " + humidityString + " WindSpeed: " + windSpeed + " Rain Ticks: " + rainTicksString + " Rain 1h: " + rain1hString + " Rain 24h: " + rain24hString + " Rain: " + rain)
 }
 
 /*
@@ -198,7 +200,7 @@ Calculates the fallen rain
 
 1 rain tick means that 5ml water fell. the ticks are absolute and reset after reaching 4096 to 0
 The area collecting the rain is 86.6cm² big
-The amount of rain in mm is calculated with (rain_ticks/2*(86.6cm²)) (blame Tony Metger if this does not work)
+The amount of rain in mm is calculated with (rainTicks/2*(86.6cm²)) (blame Tony Metger if this does not work)
 
 when a new rain_tick count is received, it
   - calculates the time passed since last update
@@ -207,61 +209,59 @@ when a new rain_tick count is received, it
   - uses math described above
   - prays to Linus Torvalds for making Linux possible (press Alt+F4 to pray now)
 
-returns rain_1h and rain_24h
- */
-func calculateRain(rain_ticks int) (float64, float64) {
+returns rain1h and rain24h
+*/
+func calculateRain(rainTicks int) (float64, float64) {
 
-  if first_data {
-    first_data = false
+	if firstData {
+		firstData = false
 
-	for i := 0; i < len(rain_1h_array); i++ {
-	  rain_1h_array[i] = rain_ticks
+		for i := 0; i < len(rain1hArray); i++ {
+			rain1hArray[i] = rainTicks
+		}
+
+		for i := 0; i < len(rain24hArray); i++ {
+			rain24hArray[i] = rainTicks
+		}
 	}
 
-	for i := 0; i < len(rain_24h_array); i++ {
-	  rain_24h_array[i] = rain_ticks
+	//time since last run
+	minutesSinceLastRun := int(time.Since(lastUpdate).Minutes())
+	//horrible hack for first run, when receiving data without passing a minute
+	if minutesSinceLastRun == 0 {
+		minutesSinceLastRun = 1
 	}
-  }
 
-  //time since last run
-  minutes_since_last_run := int(time.Since(last_update).Minutes())
-  //horrible hack for first run, when receiving data without passing a minute
-  if(minutes_since_last_run == 0) {
-    minutes_since_last_run = 1
-  }
+	lastUpdate = time.Now()
 
-  last_update = time.Now()
+	//update values in arrays
+	for i := rain1hIndex; i <= rain1hIndex+minutesSinceLastRun; i++ {
+		rain1hArray[i%len(rain1hArray)] = rainTicks
+	}
+	rain1hIndex = (rain1hIndex + minutesSinceLastRun) % len(rain1hArray)
 
+	for i := rain24hIndex; i <= rain24hIndex+minutesSinceLastRun; i++ {
+		rain24hArray[i%len(rain1hArray)] = rainTicks
+	}
+	rain24hIndex = (rain24hIndex + minutesSinceLastRun) % len(rain1hArray)
 
-  //update values in arrays
-  for i := rain_1h_index; i <= rain_1h_index + minutes_since_last_run; i++ {
-    rain_1h_array[i % len(rain_1h_array)] = rain_ticks
-  }
-  rain_1h_index = (rain_1h_index + minutes_since_last_run) % len(rain_1h_array)
+	//now takes the current rain tick and the 1/24 hour ago value and calculates the delta value, representing the fallen rain
+	rain1hDelta := rain1hArray[rain1hIndex] - rain1hArray[(rain1hIndex+1)%len(rain1hArray)]
 
-  for i := rain_24h_index; i <= rain_24h_index + minutes_since_last_run; i++ {
-    rain_24h_array[i % len(rain_1h_array)] = rain_ticks
-  }
-  rain_24h_index = (rain_24h_index + minutes_since_last_run) % len(rain_1h_array)
+	rain24hDelta := rain24hArray[rain24hIndex] - rain24hArray[(rain24hIndex+1)%len(rain24hArray)]
 
+	//calculate fallen rain
+	rain1h := float64(rain1hDelta) / (2 * 86.6)
+	rain24h := float64(rain24hDelta) / (2 * 86.6)
 
-  //now takes the current rain tick and the 1/24 hour ago value and calculates the delta value, representing the fallen rain
-  rain_1h_delta := rain_1h_array[rain_1h_index] - rain_1h_array[(rain_1h_index + 1) % len(rain_1h_array)]
-
-
-  rain_24h_delta := rain_24h_array[rain_24h_index] - rain_24h_array[(rain_24h_index + 1) % len(rain_24h_array)]
-
-  //calculate fallen rain
-  rain_1h := float64(rain_1h_delta) / (2*86.6)
-  rain_24h := float64(rain_24h_delta) / (2*86.6)
-
-  return rain_1h,rain_24h
+	return rain1h, rain24h
 }
 
-func ParseConfig(configFile string) (error, Config) {
-  var cfg Config
+//ParseConfig does exactly what the name says...
+func ParseConfig(configFile string) (Config, error) {
+	var cfg Config
 
-  err := gcfg.ReadFileInto(&cfg, configFile)
+	err := gcfg.ReadFileInto(&cfg, configFile)
 
-  return err, cfg
+	return cfg, err
 }
